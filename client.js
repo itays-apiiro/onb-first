cf6ce1e4dea3d44c50e04586253a4c18145de21c
itay segev <itay.segev@apiiro.com>
2025-08-27 08:55:53 +0300

    bla

---
+++ client.js
@@ -0,0 +1,86 @@
+const HOST="http://localhost:5555";
+
+async function run(){
+    let res1=await fetch(`${HOST}/posts`,{
+        method:'POST',
+        headers:{"Content-Type":"application/json"},
+        body:JSON.stringify({"Title":"Harry Potter1","Author":"JKRolling1@gmail.com","Creation Date":new Date(1999,4,2),"Content":"A book about harry potter"})
+    })
+    const post1 = await res1.json();
+    console.log(post1)
+
+    let res2=await fetch(`${HOST}/posts`,{
+        method:'POST',
+        headers:{"Content-Type":"application/json"},
+        body:JSON.stringify({"Title":"Harry Potter2","Author":"JKRolling2@gmail.com","Creation Date":new Date(2000,4,12),"Content":"A book about harry potter"})
+    })
+    const post2 = await res2.json();
+    console.log(post2)
+
+    let res3=await fetch(`${HOST}/posts`,{
+        method:'POST',
+        headers:{"Content-Type":"application/json"},
+        body:JSON.stringify({"Title":"Harry Potter3","Author":"JKRolling3@gmail.com","Creation Date":new Date(2005,5,17),"Content":"A book about harry potter"})
+    })
+    const post3 = await res3.json();
+    console.log(post3)
+
+
+    let res4=await fetch(`${HOST}/posts`,{
+        method:'GET',
+        headers:{"Content-Type":"application/json"},
+    })
+    const allPosts = await res4.json();
+    console.log("All posts with real IDs:", allPosts)
+
+    const idbook = post1._id
+    let res5=await fetch(`${HOST}/posts/${idbook}`,{
+        method:'GET',
+        headers:{"Content-Type":"application/json"},
+    })
+    const foundPost = await res5.json();
+    console.log(foundPost)
+
+    const Auth="JKRolling2@gmail.com"
+    let res6=await fetch(`${HOST}/posts/author/${Auth}`,{
+        method:'GET',
+        headers:{"Content-Type":"application/json"},
+    })
+    const postsByAuthor = await res6.json();
+    console.log(postsByAuthor)
+
+    Title="Harry Potter2"
+    let res7=await fetch(`${HOST}/posts/title/${Title}`,{
+        method:'GET',
+        headers:{"Content-Type":"application/json"},
+    })
+    const postByTitle = await res7.json();
+    console.log(postByTitle)
+
+    const idbook1 = post1._id
+    const idbook2 = post2._id
+    const idbook3 = post3._id
+
+    let res8=await fetch(`${HOST}/posts/${idbook1}`,{
+        method:'DELETE',
+        headers:{"Content-Type":"application/json"},
+    })
+    const deleteResult1 = await res8.json();
+    console.log(deleteResult1)
+
+    let res9=await fetch(`${HOST}/posts/${idbook2}`,{
+        method:'DELETE',
+        headers:{"Content-Type":"application/json"},
+    })
+    const deleteResult2 = await res9.json();
+    console.log(deleteResult2)
+
+    let res=await fetch(`${HOST}/posts/${idbook3}`,{
+        method:'DELETE',
+        headers:{"Content-Type":"application/json"},
+    })
+    const deleteResult3 = await res.json();
+    console.log(deleteResult3)
+
+}
+run()
---
+++ server.js
@@ -0,0 +1,137 @@
+//connection to the api (creating server)
+const express = require('express');
+const app=express();
+app.use(express.json());//to parse json request body
+const port=5555;
+//mongoDb
+const {MongoClient, ObjectId}=require('mongodb');
+const uri = "mongodb+srv://itaysegev:RQP8pfWzh7NHSR2G@cluster0.sh1plvg.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
+const client=new MongoClient(uri);
+const validator = require('validator');
+
+function validEmail(email){
+     return typeof email === 'string' && validator.isEmail(email);
+}
+
+function connectToMongoClient(db) {
+        client.connect();
+        console.log("Connected to MongoDB");
+        return client.db(db);
+}
+
+function disconnectToMongoClient(db) {
+    return ""//client.close();
+}
+
+
+app.listen(port,()=>{console.log("server listening on port:",port)})
+
+app.get("/posts",async (req,res)=>{
+    try {
+
+        const db=connectToMongoClient("onb")
+        posts = await db.collection("posts").find().sort("Creation Date","asc").toArray()
+        res.json(posts)
+        console.log(disconnectToMongoClient())
+    }catch(e){
+        res.status(500).json({error:e.message})
+        console.log("error in get posts")
+    }
+})
+
+app.get("/posts/:id",async (req,res)=>{
+    try {
+        if (!ObjectId.isValid(req.params.id)) {
+            return res.status(400).json({error: "Invalid ObjectId format"})
+        }
+        const db=connectToMongoClient("onb")
+        post = await db.collection("posts").find({"_id":new ObjectId(req.params.id)}).toArray()
+        res.json(post)
+        console.log(disconnectToMongoClient())
+    }catch(e){
+        res.status(500).json({error:e.message})
+        console.log("error in get post by id")
+    }
+})
+
+app.get("/posts/author/:Author",async (req,res)=>{
+    try {
+        const db=connectToMongoClient("onb")
+        post = await db.collection("posts").find({"Author":req.params.Author}).toArray()
+        res.json(post)
+        console.log(disconnectToMongoClient())
+    }catch(e){
+        res.status(500).json({error:e.message})
+        console.log("error in get post by author")
+    }
+})
+
+app.delete("/posts/:id",async (req,res)=>{
+    try {
+        if (!ObjectId.isValid(req.params.id)) {
+            return res.status(400).json({error: "Invalid ObjectId format"})
+        }
+        const db = connectToMongoClient("onb")
+        result = await db.collection("posts").deleteOne({"_id": new ObjectId(req.params.id)});
+        if (result.deletedCount === 0) {
+            res.status(404).json({"error": "No posts found"})
+        } else {
+            res.status(200).json({"accepted": "post deleted successfully"})
+        }
+        console.log(disconnectToMongoClient())
+    }catch(e){
+        res.status(500).json({error:e.message})
+        console.log("error in delete post by id")
+    }
+})
+
+app.post("/users",async (req,res)=>{
+    try{
+        const db=connectToMongoClient("onb")
+        result = await db.collection("users").insertOne(req.body)
+        user = await db.collection("users").findOne({"_id":result.insertedId})
+        res.status(201).json(user)
+        console.log("user added")
+    }catch(e){
+        res.status(500).json({error:e.message})
+        console.log("error in add user")
+    }
+    console.log(disconnectToMongoClient())
+})
+
+app.post("/posts",async (req,res)=>{
+    try{
+        const body=req.body;
+        console.log(body);
+        if(validEmail(body.Author)) {
+            const db=connectToMongoClient("onb")
+            result = await db.collection("posts").insertOne(body)
+            post = await db.collection("posts").findOne({"_id":result.insertedId})
+            res.status(201).json(post)
+            console.log("post added")
+        }else{
+            res.status(400).json({error:"Author Email not Valid: " + body.Author})
+        }
+    }catch(e){
+        res.status(500).json({error:e.message})
+        console.log("error in add post")
+    }
+    console.log(disconnectToMongoClient())
+})
+
+
+app.get("/posts/title/:Title",async (req,res)=>{
+    try{
+        const db=connectToMongoClient("onb")
+        post = await db.collection("posts").findOne({"Title":req.params.Title})
+        res.status(200).json(post)
+        console.log("post found by title")
+    }catch(e){
+        res.status(500).json({error:e.message})
+        console.log("error in get post by title")
+    }
+    console.log(disconnectToMongoClient())
+})
+
+
+
